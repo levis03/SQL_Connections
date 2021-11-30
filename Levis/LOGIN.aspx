8f00cc045bebc72865624b8e167ce811f995ccfe
levis03 <levismaina@aiesec.net>
2021-11-30 03:34:40 -0800

    connections

---
+++ Levis/LOGIN.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LOGIN.aspx.cs" Inherits="Levis.LOGIN" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-weight: 700">
+            <asp:Label ID="lblLogin" runat="server" Text="Customer Login Page!"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="lblCid" runat="server" Text="Customer ID"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtCid" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblpasswd" runat="server" Text="Password"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtpasswd" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnLogin" runat="server" OnClick="btnLogin_Click" Text="LOGIN" />
+            <br />
+            <br />
+            <asp:Label ID="lblConfirm" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Levis/LOGIN.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Levis
+{
+    public partial class LOGIN : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            StockTableAdapters.CustomersTableAdapter cta = new StockTableAdapters.CustomersTableAdapter();
+            //create data table
+            DataTable dt = new DataTable();
+            //fill data table
+         int CustID = Convert.ToInt32(txtCid.Text);
+            string password = txtpasswd.Text;
+            dt = cta.GetData(CustID, password);
+            if(dt.Rows.Count>=1)
+            {
+                lblConfirm.Text = "Login successful";
+                lblConfirm.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblConfirm.Text = "Login failure";
+                lblConfirm.ForeColor = System.Drawing.Color.Red;
+
+            }
+        }
+    }
+}
